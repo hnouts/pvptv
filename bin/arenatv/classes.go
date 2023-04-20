6bc756d8d021ee94c04c3e263b539393faa79ada
hnouts <hnouts@grandlyon.com>
2023-04-20 14:42:07 +0200

    ✨ new menu to chose class on stream page

--- bin/arenatv/classes.go
+++ bin/arenatv/classes.go
@@ -11,6 +11,7 @@ type classes struct {
 	app.Compo
 	Name           string
 	Slug           string
+	Svg			   string
 	lives          []liveStream
 	current        liveStream
 	Classic        bool
@@ -30,66 +31,79 @@ func getAllClasses() []classes {
 		{
 			Slug:    "death_knight",
 			Name:    "Death Knight",
+			Svg:	 dkSVG,
 			Classic: true,
 		},
 		{
 			Slug:    "demon_hunter",
 			Name:    "Demon Hunter",
+			Svg:	 dhSVG,
 			Classic: false,
 		},
 		{
 			Slug:    "druid",
 			Name:    "Druid",
+			Svg:	 druidSVG,
 			Classic: true,
 		},
 		{
 			Slug:    "evoker",
 			Name:    "Evoker",
+			Svg:	 evokerSVG,
 			Classic: false,
 		},
 		{
 			Slug:    "hunter",
 			Name:    "Hunter",
+			Svg:	 hunterSVG,
 			Classic: true,
 		},
 		{
 			Slug:    "mage",
 			Name:    "Mage",
+			Svg:	 mageSVG,
 			Classic: true,
 		},
 		{
 			Slug:    "monk",
 			Name:    "Monk",
+			Svg:	 monkSVG,
 			Classic: false,
 		},
 		{
 			Slug:    "paladin",
 			Name:    "Paladin",
+			Svg:	 paladinSVG,
 			Classic: true,
 		},
 		{
 			Slug:    "priest",
 			Name:    "Priest",
+			Svg:	 priestSVG,
 			Classic: true,
 		},
 		{
 			Slug:    "rogue",
 			Name:    "Rogue",
+			Svg:	 rogueSVG,
 			Classic: true,
 		},
 		{
 			Slug:    "shaman",
 			Name:    "Shaman",
+			Svg:	 shamanSVG,
 			Classic: true,
 		},
 		{
 			Slug:    "warlock",
 			Name:    "Warlock",
+			Svg:	 warlockSVG,
 			Classic: true,
 		},
 		{
 			Slug:    "warrior",
 			Name:    "Warrior",
+			Svg:	 warriorSVG,
 			Classic: true,
 		},
 	}
